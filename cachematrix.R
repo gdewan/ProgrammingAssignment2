6ea3c1cce5dbc903b3ffe7f517220ee108802015
gdewan <gdewan@gmail.com>
2021-10-18 20:30:37 -0700

    Programming Assignment 2: cached matrix inversion.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Make repeated matrix inversion fast, by caching the inverse after the first
+## call.
 
-## Write a short comment describing this function
+
+## This function creates a new "matrix" that is capable of caching its 
+## inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_matrix <- NULL
+  get <- function() x
+  set <- function(m) {
+    x <<- m
+    inverse_matrix <<- NULL
+  }
+  getInverse <- function () inverse_matrix
+  setInverse <- function(inverse) {
+    inverse_matrix <<- inverse
+  }
+  list(get = get, 
+       set = set, 
+       getInverse = getInverse, 
+       setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## This function efficiently inverts a matrix for repeated invocations for
+## the same matrix, by returning a cached copy after the first call.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_matrix = x$getInverse()
+  if (is.null(inverse_matrix)) {
+    m = x$get()
+    inverse_matrix = solve(m)
+    x$setInverse(inverse_matrix)
+  } 
+  inverse_matrix  
 }
